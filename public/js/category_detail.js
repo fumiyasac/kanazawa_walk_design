20901c91642202b2f58be4ae9023da490f67349d
Fumiya Sakai <fumiya.def.mathmatica@gmail.com>
2016-01-03 13:31:46 +0900

    Rails組み込み分＋検索結果部分のマークアップ追加

---
+++ public/js/category_detail.js
@@ -0,0 +1,16 @@
+/*
+  category_detail.js
+  
+  Fumiya Sakai(Just One Factory)
+  
+*/
+
+$(document).ready(function(){
+
+  var ARTICLE_THUMBNAILS = ".articleThumb";
+  
+  $(ARTICLE_THUMBNAILS).lazyload({
+    effect:"fadeIn"
+  });
+  
+});
